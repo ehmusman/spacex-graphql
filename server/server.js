fc1c89ee6c0e269b12ba06c8a83ebf065bca0fbf
ehmusman <ehmusman@gmail.com>
2021-04-18 19:28:02 +0500

    react is installed

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const { graphqlHTTP } = require("express-graphql")
+const app = express()
+// const schema = require("./schema/schema")
+const dotenv = require("dotenv")
+dotenv.config({ path: "../.env" })
+const cors = require("cors")
+app.use(cors())
+
+
+
+// app.use('/graphql', graphqlHTTP({
+//     schema,
+//     graphiql: true
+// }))
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`App is listening on port ${port}`))
