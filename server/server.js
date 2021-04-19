4537402b6552ba59b6a9050fc8be81128a41e0d1
ehmusman <ehmusman@gmail.com>
2021-04-19 09:11:23 +0500

    make it better

--- server/server.js
+++ server/server.js
@@ -13,16 +13,12 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }))
-if (process.env.NODE_ENV === 'production') {
-    console.log("production mode")
-    app.use(express.static(path.resolve(process.cwd(), 'client/build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(process.cwd(), 'client/build/index.html'))
-    })
-} else {
-    app.get("/", (req, res) => {
-        res.send("Api is Running")
-    })
-}
+app.use(express.static("public"))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../public/index.html'))
+})
+
+
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`App is listening on port ${port}`))
