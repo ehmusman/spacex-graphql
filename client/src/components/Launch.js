f1de69d100aabafd605e5a2a0ea0cfc17ecaed05
ehmusman <ehmusman@gmail.com>
2021-04-19 09:48:04 +0500

    make it better

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Launch = ({ id, date, success, name, img, detail }) => {
     const height = {
         width: "100%",
-        height: "15vw",
+        height: "25vw",
         objectFit: "cover"
     }
     return (
